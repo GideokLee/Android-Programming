2898c06852fce0399aaa033a87cd3a6a8bcd1409
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-07-02 23:13:22 +0900

    QuizViewmodel 구현

--- GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kiddo.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton : Button
     private lateinit var questionTextView: TextView
+    private val quizViewModel : QuizViewModel by lazy{
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_africa, true),
-        Question(R.string.question_americas,true),
-        Question(R.string.question_asia, true),
-        Question(R.string.question_oceans, true)
-    )
-
-    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         nextButton.setOnClickListener{
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -50,12 +48,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion(){
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer : Boolean){
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (userAnswer == correctAnswer){
             R.string.correct_toast
