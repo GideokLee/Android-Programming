90b44c9bb208a12de6ccf289a1fb04c645d77493
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-07-02 23:33:53 +0900

    SIS 사용하도록 구현

--- GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
@@ -10,7 +10,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
-
+private const val KEY_INDEX = "index"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
@@ -25,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -62,5 +65,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this, messageResId,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
 }
