cfeb3c9a74a7b0b16df55ba0588d9e7f8b640bf1
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-07-05 02:34:09 +0900

    컨닝하기 기능 및 화면 구현

--- GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/kiddo/geoquiz/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.kiddo.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,12 +13,15 @@ import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton : Button
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
     private val quizViewModel : QuizViewModel by lazy{
         ViewModelProvider(this).get(QuizViewModel::class.java)
     }
@@ -33,7 +38,13 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
+        cheatButton = findViewById(R.id.cheat_button)
 
+        cheatButton.setOnClickListener{
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
         nextButton.setOnClickListener{
             quizViewModel.moveToNext()
             updateQuestion()
@@ -50,6 +61,19 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            return
+        }
+
+        if(requestCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+
+    }
+
     private fun updateQuestion(){
         val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
@@ -58,10 +82,10 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer : Boolean){
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
-        val messageResId = if (userAnswer == correctAnswer){
-            R.string.correct_toast
-        }else{
-            R.string.incorrect_toast
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgement_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
 
         Toast.makeText(this, messageResId,Toast.LENGTH_SHORT).show()
